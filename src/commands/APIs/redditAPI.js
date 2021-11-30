dbfa1b3f0e9c59a15a9bd1a514c6844cf9601bf1
Sal Code <salazar34@outlook.it>
2021-11-30 20:14:50 +0100

    Changed Reddit API
    The previous Reddit API stopped working since 28/11/2021. Changed to the current one.

--- src/commands/APIs/redditAPI.js
+++ src/commands/APIs/redditAPI.js
@@ -1,22 +1,23 @@
 /* ========== REDDIT API ========== */
 // This integration will send random memes from
 // r/meme subreddit.
-const { reddit } = require('reddit.images');
+const reddit = require('reddit-image-fetcher');
 
 module.exports = {
   name: 'meme',
   category: 'APIs',
   description: '😂Sends a random meme from Reddit',
   slash: true,
+  testOnly: true,
 
   callback: async ({ interaction }) => {
     try {
-      reddit.FetchRandomMeme({ images: true }).then((data) => {
-        interaction.reply(data.image);
+      reddit.fetch({ type: 'meme' }).then((data) => {
+        interaction.reply(data[0].image);
       });
     } catch (error) {
       console.error(error);
-      await interaction.reply(
+      interaction.reply(
         `${interaction.user} we're sorry but there was an error while trying to communicate with the API. Please try-again later.`
       );
     }
